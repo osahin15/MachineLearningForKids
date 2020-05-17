fd1a0ea877d8382e938b9ec64082e549291f31ae
osahin2 <osahnn@outlook.com>
2020-05-17 16:14:00 +0300

    Eksik Dosyalar Eklendi

---
+++ MachineLearningForKids/SmartClass.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace MachineLearningForKids
+{
+    partial class SmartClass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_Test = new System.Windows.Forms.Button();
+            this.txt_Test = new System.Windows.Forms.TextBox();
+            this.pct_LampOn = new System.Windows.Forms.PictureBox();
+            this.pct_FanOn = new System.Windows.Forms.PictureBox();
+            this.pct_LampAndFan = new System.Windows.Forms.PictureBox();
+            this.btn_Train = new System.Windows.Forms.Button();
+            this.rdo_LampOff = new System.Windows.Forms.RadioButton();
+            this.rdo_LampOn = new System.Windows.Forms.RadioButton();
+            this.rdo_FanOn = new System.Windows.Forms.RadioButton();
+            this.rdo_FanOff = new System.Windows.Forms.RadioButton();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.txt_Train = new System.Windows.Forms.TextBox();
+            this.lyt_LampOff = new System.Windows.Forms.FlowLayoutPanel();
+            this.lyt_LampOn = new System.Windows.Forms.FlowLayoutPanel();
+            this.lyt_FanOn = new System.Windows.Forms.FlowLayoutPanel();
+            this.lyt_FanOff = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_LampOn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_FanOn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_LampAndFan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Azure;
+            this.panel1.Controls.Add(this.pct_LampOn);
+            this.panel1.Controls.Add(this.btn_Test);
+            this.panel1.Controls.Add(this.txt_Test);
+            this.panel1.Controls.Add(this.pct_FanOn);
+            this.panel1.Controls.Add(this.pct_LampAndFan);
+            this.panel1.Controls.Add(this.btn_Train);
+            this.panel1.Controls.Add(this.rdo_LampOff);
+            this.panel1.Controls.Add(this.rdo_LampOn);
+            this.panel1.Controls.Add(this.rdo_FanOn);
+            this.panel1.Controls.Add(this.rdo_FanOff);
+            this.panel1.Controls.Add(this.btn_Add);
+            this.panel1.Controls.Add(this.txt_Train);
+            this.panel1.Controls.Add(this.lyt_LampOff);
+            this.panel1.Controls.Add(this.lyt_LampOn);
+            this.panel1.Controls.Add(this.lyt_FanOn);
+            this.panel1.Controls.Add(this.lyt_FanOff);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(20, 87);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1203, 556);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_Test
+            // 
+            this.btn_Test.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_Test.Location = new System.Drawing.Point(817, 447);
+            this.btn_Test.Name = "btn_Test";
+            this.btn_Test.Size = new System.Drawing.Size(287, 64);
+            this.btn_Test.TabIndex = 13;
+            this.btn_Test.Text = "Learn And Test";
+            this.btn_Test.UseVisualStyleBackColor = false;
+            this.btn_Test.Click += new System.EventHandler(this.btn_Test_Click);
+            // 
+            // txt_Test
+            // 
+            this.txt_Test.Location = new System.Drawing.Point(817, 405);
+            this.txt_Test.Name = "txt_Test";
+            this.txt_Test.Size = new System.Drawing.Size(287, 22);
+            this.txt_Test.TabIndex = 12;
+            // 
+            // pct_LampOn
+            // 
+            this.pct_LampOn.Image = global::MachineLearningForKids.Properties.Resources.LightOn;
+            this.pct_LampOn.Location = new System.Drawing.Point(817, 149);
+            this.pct_LampOn.Name = "pct_LampOn";
+            this.pct_LampOn.Size = new System.Drawing.Size(287, 217);
+            this.pct_LampOn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct_LampOn.TabIndex = 10;
+            this.pct_LampOn.TabStop = false;
+            this.pct_LampOn.Visible = false;
+            // 
+            // pct_FanOn
+            // 
+            this.pct_FanOn.Image = global::MachineLearningForKids.Properties.Resources.FanOn;
+            this.pct_FanOn.Location = new System.Drawing.Point(817, 149);
+            this.pct_FanOn.Name = "pct_FanOn";
+            this.pct_FanOn.Size = new System.Drawing.Size(287, 217);
+            this.pct_FanOn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct_FanOn.TabIndex = 11;
+            this.pct_FanOn.TabStop = false;
+            this.pct_FanOn.Visible = false;
+            // 
+            // pct_LampAndFan
+            // 
+            this.pct_LampAndFan.Image = global::MachineLearningForKids.Properties.Resources.LightandFan;
+            this.pct_LampAndFan.Location = new System.Drawing.Point(817, 149);
+            this.pct_LampAndFan.Name = "pct_LampAndFan";
+            this.pct_LampAndFan.Size = new System.Drawing.Size(287, 217);
+            this.pct_LampAndFan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct_LampAndFan.TabIndex = 9;
+            this.pct_LampAndFan.TabStop = false;
+            // 
+            // btn_Train
+            // 
+            this.btn_Train.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_Train.Location = new System.Drawing.Point(60, 447);
+            this.btn_Train.Name = "btn_Train";
+            this.btn_Train.Size = new System.Drawing.Size(615, 64);
+            this.btn_Train.TabIndex = 8;
+            this.btn_Train.Text = "Train";
+            this.btn_Train.UseVisualStyleBackColor = false;
+            // 
+            // rdo_LampOff
+            // 
+            this.rdo_LampOff.AutoSize = true;
+            this.rdo_LampOff.Location = new System.Drawing.Point(582, 122);
+            this.rdo_LampOff.Name = "rdo_LampOff";
+            this.rdo_LampOff.Size = new System.Drawing.Size(83, 21);
+            this.rdo_LampOff.TabIndex = 7;
+            this.rdo_LampOff.TabStop = true;
+            this.rdo_LampOff.Text = "lamp_off";
+            this.rdo_LampOff.UseVisualStyleBackColor = true;
+            // 
+            // rdo_LampOn
+            // 
+            this.rdo_LampOn.AutoSize = true;
+            this.rdo_LampOn.Location = new System.Drawing.Point(409, 122);
+            this.rdo_LampOn.Name = "rdo_LampOn";
+            this.rdo_LampOn.Size = new System.Drawing.Size(83, 21);
+            this.rdo_LampOn.TabIndex = 6;
+            this.rdo_LampOn.TabStop = true;
+            this.rdo_LampOn.Text = "lamp_on";
+            this.rdo_LampOn.UseVisualStyleBackColor = true;
+            // 
+            // rdo_FanOn
+            // 
+            this.rdo_FanOn.AutoSize = true;
+            this.rdo_FanOn.Location = new System.Drawing.Point(247, 122);
+            this.rdo_FanOn.Name = "rdo_FanOn";
+            this.rdo_FanOn.Size = new System.Drawing.Size(73, 21);
+            this.rdo_FanOn.TabIndex = 5;
+            this.rdo_FanOn.TabStop = true;
+            this.rdo_FanOn.Text = "fan_on";
+            this.rdo_FanOn.UseVisualStyleBackColor = true;
+            // 
+            // rdo_FanOff
+            // 
+            this.rdo_FanOff.AutoSize = true;
+            this.rdo_FanOff.Location = new System.Drawing.Point(77, 122);
+            this.rdo_FanOff.Name = "rdo_FanOff";
+            this.rdo_FanOff.Size = new System.Drawing.Size(73, 21);
+            this.rdo_FanOff.TabIndex = 4;
+            this.rdo_FanOff.TabStop = true;
+            this.rdo_FanOff.Text = "fan_off";
+            this.rdo_FanOff.UseVisualStyleBackColor = true;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_Add.Location = new System.Drawing.Point(562, 32);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(113, 71);
+            this.btn_Add.TabIndex = 3;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = false;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // txt_Train
+            // 
+            this.txt_Train.Location = new System.Drawing.Point(197, 56);
+            this.txt_Train.Name = "txt_Train";
+            this.txt_Train.Size = new System.Drawing.Size(332, 22);
+            this.txt_Train.TabIndex = 2;
+            // 
+            // lyt_LampOff
+            // 
+            this.lyt_LampOff.BackColor = System.Drawing.Color.Honeydew;
+            this.lyt_LampOff.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lyt_LampOff.Location = new System.Drawing.Point(539, 149);
+            this.lyt_LampOff.Name = "lyt_LampOff";
+            this.lyt_LampOff.Size = new System.Drawing.Size(163, 260);
+            this.lyt_LampOff.TabIndex = 1;
+            // 
+            // lyt_LampOn
+            // 
+            this.lyt_LampOn.BackColor = System.Drawing.Color.Honeydew;
+            this.lyt_LampOn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lyt_LampOn.Location = new System.Drawing.Point(370, 149);
+            this.lyt_LampOn.Name = "lyt_LampOn";
+            this.lyt_LampOn.Size = new System.Drawing.Size(163, 260);
+            this.lyt_LampOn.TabIndex = 1;
+            // 
+            // lyt_FanOn
+            // 
+            this.lyt_FanOn.BackColor = System.Drawing.Color.Honeydew;
+            this.lyt_FanOn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lyt_FanOn.Location = new System.Drawing.Point(201, 149);
+            this.lyt_FanOn.Name = "lyt_FanOn";
+            this.lyt_FanOn.Size = new System.Drawing.Size(163, 260);
+            this.lyt_FanOn.TabIndex = 1;
+            // 
+            // lyt_FanOff
+            // 
+            this.lyt_FanOff.BackColor = System.Drawing.Color.Honeydew;
+            this.lyt_FanOff.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lyt_FanOff.Location = new System.Drawing.Point(32, 149);
+            this.lyt_FanOff.Name = "lyt_FanOff";
+            this.lyt_FanOff.Size = new System.Drawing.Size(163, 260);
+            this.lyt_FanOff.TabIndex = 0;
+            // 
+            // SmartClass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1243, 663);
+            this.Controls.Add(this.panel1);
+            this.Name = "SmartClass";
+            this.Text = "SmartClass";
+            this.Load += new System.EventHandler(this.SmartClass_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_LampOn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_FanOn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_LampAndFan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pct_LampAndFan;
+        private System.Windows.Forms.Button btn_Train;
+        private System.Windows.Forms.RadioButton rdo_LampOff;
+        private System.Windows.Forms.RadioButton rdo_LampOn;
+        private System.Windows.Forms.RadioButton rdo_FanOn;
+        private System.Windows.Forms.RadioButton rdo_FanOff;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.TextBox txt_Train;
+        private System.Windows.Forms.FlowLayoutPanel lyt_LampOff;
+        private System.Windows.Forms.FlowLayoutPanel lyt_LampOn;
+        private System.Windows.Forms.FlowLayoutPanel lyt_FanOn;
+        private System.Windows.Forms.FlowLayoutPanel lyt_FanOff;
+        private System.Windows.Forms.PictureBox pct_LampOn;
+        private System.Windows.Forms.PictureBox pct_FanOn;
+        private System.Windows.Forms.Button btn_Test;
+        private System.Windows.Forms.TextBox txt_Test;
+    }
+}
